4851108447275d44971952b495228e941412af13
oGabrie1 <gabrielconti5563@gmail.com>
2025-04-30 20:11:42 -0300

    Mudança de ordem link de style proprietário

--- Projeto/public/proprietário/gerenciarEspacos.php
+++ Projeto/public/proprietário/gerenciarEspacos.php
@@ -3,8 +3,8 @@
   <head>
     <meta charset="UTF-8" />
     <title>Gerenciar Espaços</title>
-    <link rel="stylesheet" href="proprietario.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="proprietario.css">
   </head>
   <body>
     <div class="sidebar">
